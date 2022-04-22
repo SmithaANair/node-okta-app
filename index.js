342977b81c2a2f50b8b6fe661d6b7c978ec5ba31
A <Smitha.A@experian.com>
2022-04-22 15:22:21 +0100

    port changed

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express')
+
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 8080
 
 app.get('/create', (req, res) => {
-   /* if (req.headers.authorization !== 'Basic QXp1cmVEaWFtb25kOmh1bnRlcjI=') {
-        res.set('WWW-Authenticate', 'Basic realm="401"')
-        res.status(401).send('Try user: AzureDiamond, password: hunter2')
-        return
+    /* if (req.headers.authorization !== 'Basic QXp1cmVEaWFtb25kOmh1bnRlcjI=') {
+    res.set('WWW-Authenticate', 'Basic realm="401"')
+    res.status(401).send('Try user: AzureDiamond, password: hunter2')
+    return
     }
-
-
+    
     const jwt = require('njwt')
     const claims = { iss: 'fun-with-jwts', sub: 'AzureDiamond' }
     const token = jwt.create(claims, 'ExperianJWTKey')
@@ -43,21 +43,40 @@ const oidc = new ExpressOIDC({
 app.use(oidc.router)
 
 app.get('/auth/settoken', oidc.ensureAuthenticated(), (req, res) => {
-    if (req.userinfo) {     // or req.isAuthenticated()
+    if (req.userinfo) { // or req.isAuthenticated()
         res.send(`Hi ${req.userinfo.name}! you are logged in`);
     } else {
-        
         const jwt = require('njwt')
         const claims = { iss: 'exp', sub: 'manish' }
         const token = jwt.create(claims, 'ExperianJWTKey')
-        token.setExpiration(new Date().getTime() + 60*1000)
+        token.setExpiration(new Date().getTime() + 60 * 1000)
 
-        res.cookie('token',token.compact(), { maxAge: 900000, httpOnly: true })
+        res.cookie('token', token.compact(), { maxAge: 900000, httpOnly: true })
 
         console.log('cookie have created successfully');
         res.send(req.headers);
     }
-   // res.send('manish1!')
+    // res.send('manish1!')
 })
 
-app.listen(port, () => console.log(`JWT server listening on port ${port}!`))
+app.get('/welcome', oidc.ensureAuthenticated(), (req, res) => {
+    if (req.isAuthenticated()) { // or req.isAuthenticated()
+        const userContext = req.userContext;
+        res.cookie('tokentest', userContext, { maxAge: 900000, httpOnly: true })
+        res.send(`Hi ${userContext.userinfo.name}! you are logged in`);
+    } else {
+        res.send(`Please enter correct credentials`);
+    }
+})
+
+/* oidc.on('ready', () => {
+    app.listen(8080, () => console.log('OIDC app started'));
+}); */
+
+//app.listen(port, () => console.log(`JWT server listening on port ${port}!`))
+
+const server = app.listen(port, () => {
+    console.log("Server started on port " + port);
+});
+  
+module.exports = server;
