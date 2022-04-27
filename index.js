4e0088d5a115396251792d33360149418e357650
Smitha A Nair <smithakutty27@gmail.com>
2022-04-27 15:46:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/auth/settoken', oidc.ensureAuthenticated(), (req, res) => {
         res.cookie('jwt', token.compact(), { maxAge: 900000, httpOnly: true })
 
         console.log('cookie have created successfully');
-        res.send(req.headers);
+        res.redirect(req.query.uri);
     }
 })
 
